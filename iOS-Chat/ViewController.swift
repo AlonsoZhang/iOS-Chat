aaf7a48046d0aafbce28f99f887f7c52e0ca71e2
AlonsoZhang <code@zhangwu.tech>
2018-04-24 16:28:27 +0800

    Update Delegate

--- iOS-Chat/ViewController.swift
+++ iOS-Chat/ViewController.swift
@@ -14,15 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var serverList: UITableView!
     @IBOutlet weak var statusSwitch: UISwitch!
     @IBOutlet weak var lineNote: UILabel!
-    
+    var server:Server = Server.init()
+    var serverBrowser:ServerBrowser = ServerBrowser.init()
+    let tableviewStyle = "serverlist"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statusSwitch.isOn = false
+        serverName.text = UIDevice.current.name
+        server.severname = UIDevice.current.name
+        //server.delegate = self as! ServerDelegate
+        server.start()
+        serverBrowser.severname = UIDevice.current.name
+        serverBrowser.delegate = self as! ServerBrowserDelegate
+        serverBrowser.start()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+//pragma mark -
+//pragma mark ServerBrowserDelegate
+    
+    func updateServerList() {
+        serverList.reloadData()
     }
 
     @IBAction func ChangeStatus(_ sender: UISwitch) {
@@ -33,5 +52,18 @@ class ViewController: UIViewController {
     
     @IBAction func ChangeSandbox(_ sender: UIButton) {
     }
+}
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)) as UITableViewCell
+        cell.textLabel?.text = "123"
+        return cell
+    }
+
 }
 
